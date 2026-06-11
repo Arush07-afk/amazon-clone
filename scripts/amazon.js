5083500664bff3b6a729609cdc184034c4152baa
Arush Garg <gargarush07@gmail.com>
2026-06-11 21:23:03 +0530

    Interactive cart quantity

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -54,31 +54,39 @@ products.forEach((product)=>{
 document.querySelector('.products-grid').innerHTML = productsHTML;
 
 
-document.querySelectorAll('.js-add-to-cart')
-    .forEach((button)=>{
-        button.addEventListener('click',()=>{
-            //console.log(button.dataset.productName); // gives the info related to data-product-name in the button
+document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
+    button.addEventListener('click',()=>{
+        //console.log(button.dataset.productName); // gives the info related to data-product-name in the button
 
-            const productId = button.dataset.productId;
+        const productId = button.dataset.productId;
 
-            let matchingItem;
-            cart.forEach((item) => {
-                if(productId === item.productId ){
-                    matchingItem=item;
-                }
-            });
-
-            if(matchingItem){
-                matchingItem.quantity+=1;
-            }else{
-
-                cart.push({
-                    productId:productId,
-                    quantity :1
-                });
+        let matchingItem;
+        cart.forEach((item) => {
+            if(productId === item.productId ){
+                matchingItem=item;
             }
-            console.log(cart);
         });
+
+        if(matchingItem){
+            matchingItem.quantity+=1;
+        }else{
+
+            cart.push({
+                productId:productId,
+                quantity :1
+            });
+        }
+
+        let cartQuantity = 0;
+        cart.forEach((item)=>{
+            cartQuantity+=item.quantity;
+        });
+
+        document.querySelector('.cart-quantity').innerHTML = cartQuantity;
+        
+        console.log(cartQuantity);
+        console.log(cart);
+    });
 });
 
 
