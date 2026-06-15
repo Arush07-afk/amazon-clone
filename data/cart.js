9235eef167bcfc9ed4c6bb7bf756031c13d17526
Arush Garg <gargarush07@gmail.com>
2026-06-15 23:31:53 +0530

    Create automated tests for addToCart

--- data/cart.js
+++ data/cart.js
@@ -1,21 +1,28 @@
-export let cart = JSON.parse(localStorage.getItem('cart'));
+export let cart;
+
+loadFromStorage();
+
+export function loadFromStorage(){
+  cart = JSON.parse(localStorage.getItem('cart'));
+
+
+  if (!cart ) {
+    cart = [{
+      productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+      quantity: 2,
+      deliveryOptionId:'1'
+    }, {
+      productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+      quantity: 1,
+      deliveryOptionId:'2'
+    }];
+    
+    saveToLocalStorage();
+  }
+}
 
 function saveToLocalStorage(){
   localStorage.setItem('cart',JSON.stringify(cart));
-}
-
-if (!cart || cart.length === 0) {
-  cart = [{
-    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity: 2,
-    deliveryOptionId:'1'
-  }, {
-    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 1,
-    deliveryOptionId:'2'
-  }];
-
-  saveToLocalStorage();
 }
 
 export function addToCart(productId){
